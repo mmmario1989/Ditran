f5c3e4798206dd10abc117e24cf7c02ee8e76ee4
mario <mmmario@foxmail.com>
2018-12-13 18:34:14 +0800

    config

---
+++ ditran-core/src/main/java/org/mz/ditran/core/conf/DitranActiveConfiguration.java
@@ -0,0 +1,14 @@
+package org.mz.ditran.core.conf;
+
+import org.mz.ditran.core.DitranAspect;
+
+/**
+ * @Author: mario
+ * @Email: mmmario@foxmail.com
+ * @Date: 2018-12-13 6:23 PM
+ * @Description:
+ */
+public class DitranActiveConfiguration extends DitranConfiguration{
+
+    private DitranAspect ditranAspect;
+}
