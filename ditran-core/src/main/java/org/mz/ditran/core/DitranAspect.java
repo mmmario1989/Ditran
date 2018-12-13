73ab49c8e552067a804d1f7b46c7b5ee7d23eae4
mario <mmmario@foxmail.com>
2018-12-13 17:29:14 +0800

    aspect

--- ditran-core/src/main/java/org/mz/ditran/core/DitranAspect.java
+++ ditran-core/src/main/java/org/mz/ditran/core/DitranAspect.java
@@ -5,7 +5,9 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 /**
  * @Author: mario
@@ -25,11 +27,19 @@ public class DitranAspect {
         DitranTransactional ditranAnn = (DitranTransactional) claz.getAnnotation(DitranTransactional.class);
         RpcType rpcType = ditranAnn.value();
         Propagation propagation = ditranAnn.propagation();
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        TransactionStatus status = platformTransactionManager.getTransaction(definition);
+        definition.setPropagationBehavior(propagation.value());
         try{
             Object res = point.proceed();
+            //todo block here waiting for passives success
+            platformTransactionManager.commit(status);
+            //todo notice zk the success
+            return res;
         }catch (Exception e){
-
+            //todo notice zk the failure
+            platformTransactionManager.rollback(status);
+            throw e;
         }
-        return null;
     }
 }
