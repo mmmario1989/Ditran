04134d4b1de58ab35fe92d57ec0a229185954dd3
mario <mmmario@foxmail.com>
2018-12-13 18:22:19 +0800

    listener

---
+++ ditran-core/src/main/java/org/mz/ditran/core/DitranListener.java
@@ -0,0 +1,17 @@
+package org.mz.ditran.core;
+
+/**
+ * @Author: mario
+ * @Email: mmmario@foxmail.com
+ * @Date: 2018-12-13 5:48 PM
+ * @Description:
+ *
+ * block thread and listen the status of giving key in zk and
+ * notify it when all the status are success
+ */
+public class DitranListener {
+
+    public static void listen(String...keys){
+
+    }
+}
